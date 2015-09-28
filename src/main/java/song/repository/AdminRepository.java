43065a15702d732beb0e05119d20191b4fcdd63a
aCoder2013 <247608980@qq.com>
2015-09-28 13:08:47 +0800

    优化后台

--- src/main/java/song/repository/AdminRepository.java
+++ src/main/java/song/repository/AdminRepository.java
@@ -1,18 +1,18 @@
 package song.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-import song.model.User;
+import song.model.Admin;
 
 /**
  * 用户相关数据库操作接口
  * Created by Song on 2015/6/12.
  */
-public interface UserRepository extends JpaRepository<User,Integer> {
+public interface AdminRepository extends JpaRepository<Admin,Integer> {
 
-    /*
+    /**
         通过邮箱和密码查询用户
         @param:email
         @param:password
      */
-    User findByEmailAndPassword(String email,String password);
+    Admin findByEmailAndPassword(String email,String password);
 }
