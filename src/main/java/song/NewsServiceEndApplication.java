45c9a90770550ad8a7bd253d492a0e08e87d6a50
aCoder2013 <247608980@qq.com>
2015-09-29 22:52:47 +0800

    Add Interceptor

--- src/main/java/song/NewsServiceEndApplication.java
+++ src/main/java/song/NewsServiceEndApplication.java
@@ -5,15 +5,23 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.context.web.SpringBootServletInitializer;
 import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
 import javax.servlet.ServletContext;
 
 @SpringBootApplication
 @EnableScheduling
-public class NewsServiceEndApplication   {
+public class NewsServiceEndApplication   extends WebMvcConfigurerAdapter {
 
 
     public static void main(String[] args) {
         SpringApplication.run(NewsServiceEndApplication.class, args);
+    }
+
+
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+
     }
 }
---
+++ src/main/java/song/interceptor/AdminAuthorizationInterceptor.java
@@ -0,0 +1,9 @@
+package song.interceptor;
+
+import org.springframework.web.servlet.handler.UserRoleAuthorizationInterceptor;
+
+/**
+ * Created by Song on 2015/9/29.
+ */
+public class AdminAuthorizationInterceptor extends UserRoleAuthorizationInterceptor {
+}
