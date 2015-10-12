bbc58b66465c239eab79aa90d5d57af498ae0db6
aCoder2013 <247608980@qq.com>
2015-10-12 13:38:18 +0800

    Delete file/code

--- src/main/java/song/NewsServiceEndApplication.java
+++ src/main/java/song/NewsServiceEndApplication.java
@@ -2,19 +2,9 @@ package song;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.builder.SpringApplicationBuilder;
-import org.springframework.boot.context.embedded.ServletRegistrationBean;
-import org.springframework.boot.context.web.SpringBootServletInitializer;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.ComponentScan;
 import org.springframework.scheduling.annotation.EnableScheduling;
-import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
-import song.interceptor.AdminAuthorizationInterceptor;
 
-import javax.servlet.ServletContext;
-import java.util.HashMap;
-import java.util.Map;
 
 @SpringBootApplication
 @EnableScheduling
@@ -26,12 +16,7 @@ public class NewsServiceEndApplication   extends WebMvcConfigurerAdapter {
     }
 
 
-    @Override
-    public void addInterceptors(InterceptorRegistry registry) {
-        registry.addInterceptor(new AdminAuthorizationInterceptor()).addPathPatterns("/admin/**")
-                .excludePathPatterns("/admin")
-                .excludePathPatterns("/admin/login");
-    }
+
 
 
 }
